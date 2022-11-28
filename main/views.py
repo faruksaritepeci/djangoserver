7cb232674f863daaaf801975e8342c50fe18ffe8
faruksaritepeci <fsaritepeci@hotmail.com>
2022-11-29 02:05:32 +0300

    set up interactive listing of tasks

--- main/views.py
+++ main/views.py
@@ -4,7 +4,10 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
+def home(response):
+    return render(response, "main/home.html", {})
+
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse(f"<h1>%s</h1></br><p>%s</p>" %(ls.name, item.text))
+    return render(response, "main/list.html", {"ls": ls})
+
